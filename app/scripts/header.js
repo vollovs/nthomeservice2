5015f5db907eac2fdcd31ec585b8626f92b98d7a
zlkca <zlk1979@hotmail.com>
2016-04-28 08:28:03 -0400

    Add login logout button

--- app/scripts/header.js
+++ app/scripts/header.js
@@ -11,14 +11,33 @@ angular.module('main')
 			$scope.redirectTo = function(a){
 				$location.path(a);
 			}
-						
+			
+			$scope.logout = function(){
+				var tokenInterval = $ms.getSessionItem('tokenInterval');
+				$ms.setSessionItem('token', '');
+				$ms.setSessionItem('user', '');
+				
+				var ti = $ms.getSessionItem('tokenInterval');
+				clearInterval(ti);
+				$ms.setSessionItem('tokenInterval','');
+				
+				//updateHeader();
+				$location.path('/');				
+			}
+			
+			$scope.isLoggedIn = function(){
+				$scope.user = $ms.getSessionItem('user');
+				if($scope.user)
+					return $scope.user.username == 'admin';
+				else
+					return false;
+			}
 			//--------------------------------------------------------------------
 			// When navigate to the home page, init() will be called to 
 			// 1. load filter from the cookie
 			// 2. send http request to get the item list and save
 			//--------------------------------------------------------------------
 			function init(){
-	            $scope.user = $ms.getSessionItem('user');
 	            
 	        	$scope.header = {
 	        			data: [{ text:'Home', val:'home', active:true, click:function(){
@@ -30,18 +49,7 @@ angular.module('main')
 	        			       { text:'Login', val:'login', active:true, click:function(){
 	        			    	   $location.path('/login');
 	        			       }},
-	        			       { text:'Logout', val:'logout', active:true, click:function(){
-	        						var tokenInterval = $ms.getSessionItem('tokenInterval');
-	        						$ms.setSessionItem('token', '');
-	        						$ms.setSessionItem('user', '');
-	        						
-	        						var ti = $ms.getSessionItem('tokenInterval');
-	        						clearInterval(ti);
-	        						$ms.setSessionItem('tokenInterval','');
-	        						
-	        						//updateHeader();
-	        						$location.path('/');
-	        			       }},
+	        			       { text:'Logout', val:'logout', active:true, click:$scope.logout},
 	        			       ],
 	        			opt:{
 	        				title:'angularZ',
